80f7f4f226ba14065d2f37782bd97f6d5c93a962
Adhitya Kusumadinatha <adhityasantika@yahoo.com>
2021-08-16 10:52:45 +0800

    Done 39 | Now 40

---
+++ resources/js/screens/admin/category/AddCategory.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container>
+    <v-form ref="form">
+      <v-text-field
+        :counter="35"
+        label="Name"
+        required
+        dark
+        v-model="name"
+      ></v-text-field>
+
+      <v-text-field label="Order" required dark v-model="order"></v-text-field>
+
+      <v-row class="mb-2">
+        <font-awesome-icon
+          icon="camera"
+          class="white--text mt-4 ml-2"
+          style="font-size: 30px; margin-right: -20px"
+        />
+        <v-file-input
+          label="Image"
+          show-size
+          dark
+          v-model="image"
+        ></v-file-input>
+      </v-row>
+
+      <v-btn color="success" class="mr-4" v-on:click="submitForm()">
+        Submit
+      </v-btn>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      name: "",
+      order: null,
+      image: null,
+    };
+  },
+
+  methods: {
+    submitForm() {
+      console.log(this.image);
+      const formData = new FormData();
+      formData.append("name", this.name);
+      formData.append("order", this.order);
+      formData.append("image", this.image);
+      axios
+        .post("http://127.0.0.1:8000/api/add/category", formData)
+        .then((response) => {
+          console.log(response.data);
+          if (response.status >= 200 && response.status < 300) {
+            this.$router.push("categories");
+          }
+        });
+    },
+  },
+};
+</script>
