81becfb91cca135307c795a7ecfb0c4867d70595
Adhitya Kusumadinatha <adhityasantika@yahoo.com>
2021-08-19 19:22:37 +0800

    Done 43 | Now 44

--- resources/js/components/admin/AppNavigator.vue
+++ resources/js/components/admin/AppNavigator.vue
@@ -21,13 +21,24 @@
       <v-btn class="ml-2" @click="() => this.$router.push('addProduct')">
           Add Products
       </v-btn>
+      <v-btn class="mx-2" fab dark medium color="red" v-on:click="logout()">
+        <font-awesome-icon icon="sign-out-alt" style="font-size:25px"></font-awesome-icon>
+      </v-btn>
     </v-toolbar>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    logout() {
+      axios.post('http://localhost:8000/admin/logout').then(response => {
+        if (response.status >= 200 && response.status < 300) {
+          this.$router.push({name: 'Login'})
+        }
+      })
+    }
+  }
 }
 </script>
 
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -49,7 +49,8 @@ export default {
         },
         {
             path: '/login',
-            component: Login
+            component: Login,
+            name: 'Login'
         }
     ]
 }
